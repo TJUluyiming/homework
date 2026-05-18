c678060bfd5bc9738158c5a4cdc2e61bf4b57cab
TJUluyiming <3075882153@qq.com>
2026-05-18 13:16:20 +0800

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -1,30 +1,33 @@
+// lexer.h
 #ifndef LEXER_H
 #define LEXER_H
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 
-// 定义种别码
-enum TokenType { KW, OP, SE, IDN, INT, FLOAT, END_OF_FILE };
+enum TokenType { KW, OP, SE, IDN, INT_CONST, FLOAT_CONST, END_OF_FILE };
 
 struct Token {
     TokenType type;
     std::string type_name;
     std::string value;
-    int type_code; 
+    int type_code;
+    int line;
 };
 
 class Lexer {
 private:
     std::string source;
     size_t pos;
+    int current_line;
     std::map<std::string, int> kw_map;
     std::map<std::string, int> op_map;
     std::map<std::string, int> se_map;
+    std::string toLower(std::string s);
 
 public:
+    std::map<std::string, Token> symbol_table; // 词法分析符号表
     Lexer(const std::string& code);
     std::vector<Token> tokenize();
 };
