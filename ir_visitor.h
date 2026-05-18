24c708508c75175a051e506fd6e89ef9776a7cd2
TJUluyiming <3075882153@qq.com>
2026-05-18 15:01:09 +0800

    Update ir_visitor.h

--- ir_visitor.h
+++ ir_visitor.h
@@ -1,6 +1,7 @@
 // ir_visitor.h
 #ifndef IR_VISITOR_H
 #define IR_VISITOR_H
+
 #include "parser.h"
 #include <vector>
 #include <map>
@@ -10,14 +11,22 @@
 
 struct Type {
     std::string name;
-    Type(std::string n = "i32") : name(n) {}
+    Type(std::string n = "i32") {
+        if (n == "int") name = "i32"; // 拦截 C-- 的 int 并转换为 LLVM 的 i32
+        else name = n;
+    }
+    bool isInt() const { return name == "i32"; }
+    bool isFloat() const { return name == "float"; }
+    bool isVoid() const { return name == "void"; }
 };
 
 struct Value {
     virtual ~Value() = default;
     virtual std::string getName() const { return ""; }
     virtual bool isConstant() const { return false; }
-    virtual int getConstantValue() const { return 0; }
+    virtual int getIntValue() const { return 0; }
+    virtual float getFloatValue() const { return 0.0f; }
+    virtual Type getType() const { return Type("i32"); }
 };
 
 struct ConstantInt : public Value {
@@ -26,102 +35,101 @@ struct ConstantInt : public Value {
     static Value* get(int v) { return new ConstantInt(v); }
     std::string getName() const override { return std::to_string(v); }
     bool isConstant() const override { return true; }
-    int getConstantValue() const override { return v; }
+    int getIntValue() const override { return v; }
+    Type getType() const override { return Type("i32"); }
+};
+
+struct ConstantFloat : public Value {
+    float v;
+    ConstantFloat(float _v) : v(_v) {}
+    static Value* get(float v) { return new ConstantFloat(v); }
+    std::string getName() const override { return std::to_string(v); }
+    bool isConstant() const override { return true; }
+    float getFloatValue() const override { return v; }
+    Type getType() const override { return Type("float"); }
 };
 
 struct GlobalVariable : public Value {
-    std::string name;
-    GlobalVariable(std::string n, Type t, bool c, Value* i) : name(n) {
-        int val = i ? static_cast<ConstantInt*>(i)->v : 0;
-        std::cout << "@" << n << " = dso_local global " << t.name << " " << val << ", align 4\n";
+    std::string name; Type type; bool is_const; Value* init;
+    GlobalVariable(std::string n, Type t, bool c, Value* i) : name(n), type(t), is_const(c), init(i) {
+        std::cout << "@" << name << " = global " << type.name << " " << (init ? init->getName() : "0") << "\n";
     }
-    static Value* create(std::string n, Type t, bool c, Value* i) {
-        return new GlobalVariable(n, t, c, i);
-    }
+    static Value* create(std::string n, Type t, bool c, Value* i) { return new GlobalVariable(n, t, c, i); }
     std::string getName() const override { return "@" + name; }
+    Type getType() const override { return type; }
 };
 
 struct AllocaInst : public Value {
-    std::string name;
-    AllocaInst(std::string n, Type t) : name(n) {
-        std::cout << "  %" << name << " = alloca " << t.name << ", align 4\n";
-    }
+    std::string name; Type type;
+    AllocaInst(std::string n, Type t) : name(n), type(t) { std::cout << "  %" << name << " = alloca " << type.name << "\n"; }
     std::string getName() const override { return "%" + name; }
-};
-
-struct LoadInst : public Value {
-    std::string name;
-    LoadInst(std::string n, Value* ptr) : name(n) {
-        std::cout << "  %" << name << " = load i32, i32* " << ptr->getName() << ", align 4\n";
-    }
-    std::string getName() const override { return "%" + name; }
+    Type getType() const override { return type; }
 };
 
 struct StoreInst : public Value {
     StoreInst(Value* val, Value* ptr) {
-        std::cout << "  store i32 " << val->getName() << ", i32* " << ptr->getName() << ", align 4\n";
+        std::cout << "  store " << val->getType().name << " " << val->getName() << ", " << ptr->getType().name << "* " << ptr->getName() << "\n";
     }
 };
 
-struct BinaryOpInst : public Value {
-    std::string name;
-    BinaryOpInst(std::string n, std::string op, Value* lhs, Value* rhs) : name(n) {
-        std::cout << "  %" << name << " = " << op << " i32 " << lhs->getName() << ", " << rhs->getName() << "\n";
+struct LoadInst : public Value {
+    std::string name; Type type;
+    LoadInst(std::string n, Value* ptr) : name(n), type(ptr->getType()) {
+        std::cout << "  %" << name << " = load " << type.name << ", " << type.name << "* " << ptr->getName() << "\n";
     }
     std::string getName() const override { return "%" + name; }
+    Type getType() const override { return type; }
 };
 
-struct ICmpInst : public Value {
-    std::string name;
-    ICmpInst(std::string n, std::string pred, Value* lhs, Value* rhs) : name(n) {
-        std::cout << "  %" << name << " = icmp " << pred << " i32 " << lhs->getName() << ", " << rhs->getName() << "\n";
+struct BinaryInst : public Value {
+    std::string name; Type type;
+    BinaryInst(std::string n, std::string op, Value* lhs, Value* rhs) : name(n), type(lhs->getType()) {
+        std::cout << "  %" << name << " = " << op << " " << type.name << " " << lhs->getName() << ", " << rhs->getName() << "\n";
     }
     std::string getName() const override { return "%" + name; }
+    Type getType() const override { return type; }
 };
 
 struct BranchInst : public Value {
-    BranchInst(std::string dest) {
-        std::cout << "  br label %" << dest << "\n";
-    }
-    BranchInst(Value* cond, std::string true_dest, std::string false_dest) {
-        std::cout << "  br i1 " << cond->getName() << ", label %" << true_dest << ", label %" << false_dest << "\n";
+    BranchInst(std::string label) { std::cout << "  br label %" << label << "\n"; }
+    BranchInst(Value* cond, std::string t_lbl, std::string f_lbl) {
+        std::cout << "  br i1 " << cond->getName() << ", label %" << t_lbl << ", label %" << f_lbl << "\n";
     }
 };
 
-struct LabelInst : public Value {
-    LabelInst(std::string name) {
-        std::cout << "\n" << name << ":\n";
-    }
-};
+struct LabelInst : public Value { LabelInst(std::string lbl) { std::cout << lbl << ":\n"; } };
 
 struct ReturnInst : public Value {
-    ReturnInst(Value* val) {
-        std::cout << "  ret i32 " << val->getName() << "\n";
-    }
-    ReturnInst() {
-        std::cout << "  ret void\n";
+    ReturnInst(Value* v = nullptr) {
+        if (v) std::cout << "  ret " << v->getType().name << " " << v->getName() << "\n";
+        else std::cout << "  ret void\n";
     }
 };
 
-struct Function : public Value {
-    std::string name;
-    Function(std::string n, Type t, const std::vector<std::string>& params = {}) : name(n) {
-        std::cout << "\ndefine " << t.name << " @" << n << "(";
-        for (size_t i = 0; i < params.size(); i++) {
-            if (i > 0) std::cout << ", ";
-            std::cout << "i32 %" << params[i];
-        }
-        std::cout << ") {\n";
-    }
-    static Value* create(std::string n, Type t, const std::vector<std::string>& params = {}) {
-        return new Function(n, t, params);
-    }
+struct FunctionValue : public Value {
+    std::string name; Type ret_type;
+    FunctionValue(std::string n, Type t) : name(n), ret_type(t) {}
     std::string getName() const override { return "@" + name; }
+    Type getType() const override { return ret_type; }
+};
+
+struct CallInst : public Value {
+    std::string name; Type return_type;
+    CallInst(std::string n, Value* func, const std::vector<Value*>& args = {}) : name(n), return_type(func->getType()) {
+        std::cout << "  %" << name << " = call " << return_type.name << " " << func->getName() << "(";
+        for (size_t i = 0; i < args.size(); i++) {
+            if (i > 0) std::cout << ", ";
+            std::cout << args[i]->getType().name << " " << args[i]->getName();
+        }
+        std::cout << ")\n";
+    }
+    std::string getName() const override { return "%" + name; }
+    Type getType() const override { return return_type; }
 };
 
 class IRVisitor {
 private:
-    std::vector<std::map<std::string, Value*>> symbol_scopes; // 支持作用域的符号表
+    std::vector<std::map<std::string, Value*>> symbol_scopes;
     int temp_counter = 0;
     int label_counter = 0;
     std::string newTemp() { return "tmp" + std::to_string(temp_counter++); }
@@ -138,8 +146,14 @@ private:
     Value* visitEqExp(ParseTree* node);
     Value* visitLAndExp(ParseTree* node);
     Value* visitLOrExp(ParseTree* node);
+    
+    void extractVarDefs(ParseTree* node, std::vector<ParseTree*>& defs);
+    void extractConstDefs(ParseTree* node, std::vector<ParseTree*>& defs);
+    void extractFuncFParams(ParseTree* node, std::vector<ParseTree*>& params);
+    void extractFuncRParams(ParseTree* node, std::vector<ParseTree*>& params); // 新增
 
 public:
+    IRVisitor(); // <--- 新增构造函数声明
     void visit(ParseTree* node);
 };
 
